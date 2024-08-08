0bb2f4518102af5d3c11647c4c0effe137bbb72f
LucassCassaretto <lucascassaretto2@gmail.com>
2024-08-08 01:00:03 -0300

    Ejercicio 1 y 2

---
+++ Ejercicio 2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Dada las edades de 20 personas guardadas en una lista, imprimir por pantalla cuántos son mayores de edad y cuántos no.
+
+
+List<int> personasEdades = new List<int> { 15, 17, 20, 14, 25, 18, 19, 16, 21, 23, 22, 30, 13, 29, 24, 27, 28, 26, 32, 12 };
+
+int mayor = 0;
+int menor = 0;
+
+foreach (int a in personasEdades)
+{
+    if (a >= 18) mayor++;
+    else menor++;
+}
+
+Console.WriteLine("La cantidad de personas mayores es de: " +  mayor);
+Console.WriteLine("La cantidad de personas menores es de: " +  menor);
--- EjerciciosOpcionales2/Program.cs
+++ EjerciciosOpcionales2/Program.cs
@@ -1,1 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+
+//Dado que se tiene almacenado en una lista, los resultados de los últimos 10 exámenes de un alumno,
+//calcular su promedio y mostrar por pantalla las 10 notas de los exámenes y el promedio resultante.
+
+
+List<int> notas = new List<int>();
+
+    notas.Add(7);
+    notas.Add(10);
+    notas.Add(9);
+    notas.Add(10);
+    notas.Add(10);
+    notas.Add(8);
+    notas.Add(4);
+    notas.Add(5);
+    notas.Add(4);
+    notas.Add(4);
+
+double suma = 0;
+int cont = 0;
+
+foreach (int i in notas)
+{   
+    cont++;
+   Console.WriteLine("Nota del parcial: "+cont + " es: " +i);
+    suma += i;
+}
+
+double cantidadDeNotas = notas.Count;
+
+double promedio = suma / cantidadDeNotas ;
+
+Console.WriteLine("La nota promedio de todas las notas es : " + promedio );
