6c3e641fa14665809297b0805cdeb7ab115aeca1
LucassCassaretto <lucascassaretto2@gmail.com>
2024-08-09 17:49:47 -0300

    Ejericicios 7 y 8 completos, falta validaciones

---
+++ Ejercicio 7/Program.cs
@@ -0,0 +1,63 @@
+﻿/*  Almacenar en una matriz las tablas del 1 al 9, teniendo en cuenta que en la primera fila y la primera columna se debe guardar los números (de 0 a 9),
+  estando el cero en la primera posición (fila 0, columna 0). 
+  El resto de los lugares debe ser calculado usando los números que se dispone, por ejemplo, en la fila 1, calcular 1*1, 1*2, 1*3, etc.
+  usando las posiciones del array o arreglo. Al finalizar el cálculo, mostrar la matriz por pantalla
+*/
+
+
+
+int[,] tablas = new int [10,11];
+int calculo=0;
+
+for (int i = 0; i< tablas.GetLength(0); i++)
+{
+    for (int j = 0; j < tablas.GetLength(1); j++)
+    {
+        if (i == 0 && j == 0) tablas[i, j] = 0;
+        else if (i == 0)
+        {
+            tablas[i, j] = j;
+        }
+    }
+  
+        tablas[i, 0] = i;
+   
+}
+
+
+for (int i = 1; i < tablas.GetLength(0); i++)
+{
+    for (int j = 1; j < tablas.GetLength(1); j++)
+    {
+        calculo = calcularTablas(tablas, j, i);
+
+        tablas[i, j] = calculo;
+    }
+
+    tablas[i, 0] = i;
+
+}
+
+
+
+
+for (int i = 0; i < tablas.GetLength(0); i++)
+{
+    for (int j = 0; j < tablas.GetLength(1); j++)
+    {
+        Console.Write(tablas[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+
+
+
+static int calcularTablas(int[,] vec, int j, int i)
+{
+    int fila = vec[i,0];
+    int columna = vec[0,j];
+
+    int multiplicacion= fila * columna;
+
+    return multiplicacion;
+}
---
+++ Ejercicio 8/Program.cs
@@ -0,0 +1,66 @@
+﻿/*Crear una matriz de 10 x 10, y “esconder” varias ‘X’ en lugares aleatorios (la cantidad que el programador decida, pero no más de la mitad de los lugares disponibles en la matriz). 
+ * El usuario deberá ingresar el lugar donde cree que hay una X, ingresando la fila y la columna por separado.
+ * Informar si acertó o no por cada ingreso. Se debe pedir al usuario ingresar valores por tantas X que se haya guardado. 
+ * El usuario tiene 3 intentos para fallar. Al finalizar (Ya sea porque se terminaron los 3 intentos,
+ * o el jugador acertó todas las X) imprimir por pantalla la matriz con sus correspondientes X, mostrando un * donde no haya nada.
+ */
+string[,] matriz = new string[10, 10];
+int cont = 0;
+while (true)
+{
+
+    Console.Write("Ingrese la posicion X: ");
+    int.TryParse(Console.ReadLine(), out int X);
+
+    Console.Write("Ingrese la posicion Y: ");
+    int.TryParse(Console.ReadLine(), out int Y);
+
+
+    
+
+    for (int i = 0; i < matriz.GetLength(0); i++)
+    {
+        for (int j = 0; j < matriz.GetLength(1); j++)
+        {
+            matriz[i, j] = "O";
+
+            matriz[8, 4] = "X";
+            matriz[2, 4] = "X";
+            matriz[7, 3] = "X";
+            matriz[1, 1] = "X";
+            matriz[2, 5] = "X";
+            matriz[2, 4] = "X";
+            matriz[5, 6] = "X";
+            matriz[1, 4] = "X";
+
+
+        }
+
+    }
+
+    for (int i = 0; i < matriz.GetLength(0); i++)
+    {
+        for (int j = 0; j < matriz.GetLength(1); j++)
+        {
+             if (string.Equals(matriz[X, Y],"X"))
+            {
+                Console.WriteLine("X encontada!");
+                cont=1;
+                break;
+            }
+        }
+        if (cont == 1) break;
+    }
+    if (cont == 1) break;
+    else Console.WriteLine("VUELVE A INTENTARLO");
+}
+
+
+for (int i = 0;  i < matriz.GetLength(0); i++)
+{
+      for (int j = 0;j < matriz.GetLength(1); j++)
+    {
+        Console.Write(matriz[i, j] + "\t");
+    }
+      Console.WriteLine();
+}
