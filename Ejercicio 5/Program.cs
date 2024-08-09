31806112d14bfe37f382577c312ec8c789f4ffc7
LucassCassaretto <lucascassaretto2@gmail.com>
2024-08-09 00:31:35 -0300

    ejercicio 5

---
+++ Ejercicio 5/Program.cs
@@ -0,0 +1,28 @@
+﻿string[,] matriz = new string[5,5];
+int cont = 0;
+
+for(int i=0; i<matriz.GetLength(0); i++)
+{
+    for(int f=0; f < matriz.GetLength(1); f++)
+    {   
+        cont++;
+        if (cont % 2 != 0)
+        {
+            matriz[i, f] = "i";
+        }
+        else matriz[i, f] = "p";
+    }
+
+}
+
+
+
+for (int i = 0; i < matriz.GetLength(0); i++)
+{
+    for (int f = 0; f < matriz.GetLength(1); f++)
+    {
+        Console.Write(matriz[i,f] + "\t");
+    }
+    Console.WriteLine();
+
+}
